de3db3c1a8f8514dc544d4558042baeee6aec3b6
asiwen <pww007@gmail.com>
2014-06-22 23:37:53 +0800

    comlete the script

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,51 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+## Create a cacheable object relating to a matrix which specificed by the argument x.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  inv <- NULL
+  set <- function(y){
+    x <<- y
+    inv <<- NULL
+  }
+  
+  get <- function() x
+  
+  setinv <- function(z){
+    inv <<- z
+  }
+  
+  getinv <- function() inv
+  
+  list(set = set, get = get, getinv = getinv, setinv = setinv)
 }
 
 
-## Write a short comment describing this function
+## Sovle a inverse of a matrix specified by the cacheable object x, 
+## which is created by the makeCacheMatrix function.
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  inv <- x$getinv()
+  if(!is.null(inv)){
+    message('get a inverse from the cache')
+    return(inv)
+  }
+  
+  ## Retrieve the original matrix of which inverse would be solved.
+  m <- x$get()
+  
+  ## Actually, not all matrices have inverses.
+  ## In order to make this script run properly here,suppose the matrix
+  ## was non-invertible if an error has been raise with the solve function
+  ## invoked and define the inverse is NULL.
+  r <-try(solve(m), silent = TRUE)
+  if('try-error' == class(r)){
+    x$setinv(NULL)
+    return(NULL)
+  }
+  x$setinv(r)
+  r
 }
+
